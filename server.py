b59290404296777a2cbdfc1764a4a694f546ac29
sskks <112801768+sskks@users.noreply.github.com>
2026-06-26 18:12:38 +0800

    feat: 收口今晚线上测试版本

--- server.py
+++ server.py
@@ -19,7 +19,7 @@ class AppHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header("Access-Control-Allow-Origin", "*")
         self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-        self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type, X-DashScope-Api-Key")
         super().end_headers()
 
     def do_OPTIONS(self):
